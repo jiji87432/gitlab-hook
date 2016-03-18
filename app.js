2089701746acb4928446b0215df63f791e745cc0
刘海铭 <liuhaiming.cn@gmail.com>
2016-03-18 10:44:43 +0800

    fix bug

--- app.js
+++ app.js
@@ -11,25 +11,30 @@ http.createServer(function (req, res) {
 	    });
 	    req.on("end",function(){
 	        let data= Buffer.concat(arr).toString();
-	        let ret;
+	        let ret = {};
 	        try{
 	            ret = JSON.parse(data);
 	        }catch(err){
-	        	result = 'JSON parse error, data: ' + data;
+	        	console.log('JSON parse error, data: ' + data);
 	        }
 
 	        // 当提交的分支是develop时触发脚本
-	        if(ret && ret.ref === 'refs/heads/develop') {
+	        if(ret.ref === 'refs/heads/develop') {
 	        	console.log('%s auto deploy', new Date().toLocaleString());
-				child_process.exec('sh ' + __dirname + '/hook.sh', function (err, stdout, stderr) {
+	        	let options = {
+					encoding: 'utf8',
+				  	timeout:60000
+				};
+				child_process.exec('sh ' + __dirname + '/hook.sh', options, function (err, stdout, stderr) {
 				    console.log(err || stdout || stderr);
 				});
-	        }
+	        } else {
+                console.log('skip ref:' + ret.ref);
+            }
 	    })
 	} else {
 		result = 'Error:404';
 	}
-	console.log(result);
   	res.end(result);
 }).listen(8081, function () {
   console.log('hook server is start');
